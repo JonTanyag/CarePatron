f6f3ada35a9141ca609bc0aae415b5ab8cf0c29f
Jon <tanyagjonjon@gmail.com>
2023-10-10 15:59:21 +0800

    implement CRUD endpoints and create ClientService to handle custom logic

--- api/Program.cs
+++ api/Program.cs
@@ -1,5 +1,7 @@
 ﻿using api.Data;
+using api.Models;
 using api.Repositories;
+using api.Services;
 using Microsoft.EntityFrameworkCore;
 
 var builder = WebApplication.CreateBuilder(args);
@@ -30,6 +32,7 @@ services.AddScoped<DataSeeder>();
 services.AddScoped<IClientRepository, ClientRepository>();
 services.AddScoped<IEmailRepository, EmailRepository>();
 services.AddScoped<IDocumentRepository, DocumentRepository>();
+services.AddScoped<IClientService, ClientService>();
 
 var app = builder.Build();
 
@@ -41,11 +44,33 @@ if (app.Environment.IsDevelopment())
     app.UseSwaggerUI();
 }
 
-app.MapGet("/clients", async (IClientRepository clientRepository) =>
+app.MapGet("/clients", async (IClientService _clientService) =>
 {
-    return await clientRepository.Get();
+    return await _clientService.Get();
 })
 .WithName("get clients");
+
+app.MapGet("/api/clients/{searchParam}", async (IClientService _clientService, string searchParam) =>
+{
+    var clients = await _clientService.Search(searchParam);
+    if (clients == null)
+    {
+        return Results.NotFound($"Clients not found.");
+    }
+
+    return Results.Ok(clients);
+});
+
+app.MapPost("/clients", async (IClientService _clientService, Client client) =>
+{
+    await _clientService.Create(client);
+});
+
+app.MapPut("/clients/{id}", async (IClientService _clientService, string id, Client client) =>
+{
+    await _clientService.Update(client);
+});
+
 
 app.UseCors();
 
---
+++ api/Services/ClientService.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Text.RegularExpressions;
+using api.Models;
+using api.Repositories;
+
+namespace api.Services
+{
+	public interface IClientService
+	{
+        Task<Client[]> Search(string searchParam);
+        Task<Client[]> Get();
+        Task Create(Client client);
+        Task Update(Client client);
+    }
+
+	public class ClientService : IClientService
+	{
+        private readonly IClientRepository _clientRepository;
+		public ClientService(IClientRepository clientRepository)
+		{
+            _clientRepository = clientRepository;
+		}
+
+        public async Task Create(Client client)
+        {
+            if (!ValidateEmail(client.Email))
+                throw new Exception("Email address provided is not valid.");
+
+            await _clientRepository.Create(client);
+        }
+
+        public async Task<Client[]> Search(string searchParam)
+        {
+            var clients = await _clientRepository.Get();
+
+            if (clients != null)
+            {
+                var results = clients.Where(x => x.FirstName.Contains(searchParam, StringComparison.OrdinalIgnoreCase)
+                    || x.LastName.Contains(searchParam, StringComparison.OrdinalIgnoreCase)).ToArray();
+                return results;
+            }
+
+            return new Client[0];
+        }
+
+        public async Task<Client[]> Get()
+        {
+            var clients = await _clientRepository.Get();
+
+            return clients;
+        }
+
+        public async Task Update(Client client)
+        {
+            if (!ValidateEmail(client.Email))
+                throw new Exception("Email address provided is not valid.");
+
+            await _clientRepository.Update(client);
+        }
+
+        private bool ValidateEmail(string email)
+        {
+            // Define a regular expression pattern for email validation
+            string pattern = @"^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$";
+
+            // Create a Regex object and match the email against the pattern
+            Regex regex = new Regex(pattern);
+            Match match = regex.Match(email);
+
+            // Check if the email matches the pattern
+            return match.Success;
+        }
+    }
+}
+
